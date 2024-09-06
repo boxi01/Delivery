91ed14a8a806e881e4e150c30f1066310995e833
jpude <joannapudelko95@gmail.com>
2024-09-06 21:31:40 +0200

    Add start page

---
+++ src/pages/start-page/start-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-page.component.html',
+  styleUrl: './start-page.component.scss'
+})
+export class StartPageComponent {
+  
+  onClickSend() {
+    // TODO
+  }
+}
